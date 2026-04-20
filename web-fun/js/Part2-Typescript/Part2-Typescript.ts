aef005cb0dbb60a900ec6179f4e45433decebae7
muraddavid97-eng <muraddavid97@gmail.com>
2026-04-20 22:28:19 +0300

    part2

---
+++ web-fun/js/Part2-Typescript/Part2-Typescript.ts
@@ -0,0 +1,19 @@
+interface Student {
+  name: string;
+  percentage: number;
+}
+
+function printProgress(student: Student): void {
+  console.log(student.name + ' has completed ' + student.percentage + '% of the bootcamp.');
+}
+
+const student1: Student = {
+  name: 'Alice',
+  percentage: 80
+};
+
+printProgress(student1);
+
+// This will give an error
+// printProgress({ name: 'Bob' });
+printProgress({ name: 'Bob', percentage: 60 });
