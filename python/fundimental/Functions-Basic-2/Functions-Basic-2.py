8880588d4ccad8c984f3df0b1bcecb456bbf168b
muraddavid97-eng <muraddavid97@gmail.com>
2026-05-04 16:04:16 +0300

    fun2

---
+++ python/fundimental/Functions-Basic-2/Functions-Basic-2.py
@@ -0,0 +1,41 @@
+# 1. Countdown
+def countdown(num):
+    result = []
+    for i in range(num, -1, -1):
+        result.append(i)
+    return result
+
+
+# 2. Print and Return
+def print_and_return(lst):
+    print(lst[0])
+    return lst[1]
+
+
+# 3. First Plus Length
+def first_plus_length(lst):
+    return lst[0] + len(lst)
+
+
+# 4. Values Greater than Second
+def values_greater_than_second(lst):
+    if len(lst) < 2:
+        return False
+    
+    second = lst[1]
+    result = []
+
+    for num in lst:
+        if num > second:
+            result.append(num)
+
+    print(len(result))
+    return result
+
+
+# 5. This Length, That Value
+def length_and_value(size, value):
+    result = []
+    for i in range(size):
+        result.append(value)
+    return result
---
+++ python/fundimental/Functions-Basic-2/readme.txt
@@ -0,0 +1,14 @@
+#Functions Basic II
+
+This assignment helped me understand how to work with functions, lists, and loops in Python.
+
+I practiced:
+Creating and returning lists
+Using loops to build data
+Working with conditions
+Understanding how functions return values
+
+The most important part was learning how to think step by step:
+input → process → output
+
+This made functions much clearer and easier to use.
