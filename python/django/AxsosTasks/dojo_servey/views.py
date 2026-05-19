4bd7c1fe7b6981e12e6d5a7ffd8534d06c42d19c
muraddavid97-eng <muraddavid97@gmail.com>
2026-05-19 19:23:20 +0300

    lastone

--- python/django/AxsosTasks/dojo_servey/views.py
+++ python/django/AxsosTasks/dojo_servey/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse("Hello from Dojo Survey")
+
+
+
+
+
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, "index.html")
+
+def result(request):
+    if request.method == "POST":
+        context = {
+            "name": request.POST["name"],
+            "location": request.POST["location"],
+            "language": request.POST["language"],
+            "comment": request.POST["comment"],
+        }
+        return render(request, "result.html", context)
+
+    return redirect("/")
