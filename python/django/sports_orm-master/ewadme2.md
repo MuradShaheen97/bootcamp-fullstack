ea5d0cff77fabb8c60326553b33bebf1266c56be
muraddavid97-eng <muraddavid97@gmail.com>
2026-06-02 15:27:16 +0300

    orm2

---
+++ python/django/sports_orm-master/ewadme2.md
@@ -0,0 +1,30 @@
+# Sports ORM II
+
+## Overview
+
+This project is a Django ORM assignment focused on querying related data between leagues, teams, and players using ForeignKey and ManyToMany relationships.
+
+## Technologies
+
+- Python
+- Django
+- SQLite
+- Django ORM
+
+## Screenshots
+
+### Home Page
+
+![Home Page](img/photo5.png)
+
+### Query Results
+
+![Query Results](img/photo6.png)
+
+## What I Learned
+
+- Using `filter()` and `exclude()`
+- Querying through relationships
+- ForeignKey and ManyToMany fields
+- Using `annotate()` and `Count()`
+- Displaying ORM query results in templates
--- python/django/sports_orm-master/leagues/views.py
+++ python/django/sports_orm-master/leagues/views.py
@@ -1,24 +1,53 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import League, Team, Player
+
 
 def index(request):
     context = {
-        "baseball_leagues": League.objects.filter(sport="Baseball"),
-        "women_leagues": League.objects.filter(name__contains="Women"),
-        "hockey_leagues": League.objects.filter(sport__contains="Hockey"),
-        "not_football_leagues": League.objects.exclude(sport="Football"),
-        "conference_leagues": League.objects.filter(name__contains="Conference"),
-        "atlantic_leagues": League.objects.filter(name__contains="Atlantic"),
-        "dallas_teams": Team.objects.filter(location="Dallas"),
-        "raptors_teams": Team.objects.filter(team_name="Raptors"),
-        "city_teams": Team.objects.filter(location__contains="City"),
-        "t_teams": Team.objects.filter(team_name__startswith="T"),
-        "teams_by_location": Team.objects.order_by("location"),
-        "teams_by_name_reverse": Team.objects.order_by("-team_name"),
-        "cooper_players": Player.objects.filter(last_name="Cooper"),
-        "joshua_players": Player.objects.filter(first_name="Joshua"),
-        "cooper_not_joshua": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
-        "alexander_or_wyatt": Player.objects.filter(first_name="Alexander") | Player.objects.filter(first_name="Wyatt"),
+        # Sports ORM II
+        "q1": Team.objects.filter(league__name="Atlantic Soccer Conference"),
+        "q2": Player.objects.filter(curr_team__location="Boston", curr_team__team_name="Penguins"),
+        "q3": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
+        "q4": Player.objects.filter(
+            curr_team__league__name="American Conference of Amateur Football",
+            last_name="Lopez"
+        ),
+        "q5": Player.objects.filter(curr_team__league__sport="Football"),
+        "q6": Team.objects.filter(curr_players__first_name="Sophia"),
+        "q7": League.objects.filter(teams__curr_players__first_name="Sophia"),
+        "q8": Player.objects.filter(last_name="Flores").exclude(
+            curr_team__location="Washington",
+            curr_team__team_name="Roughriders"
+        ),
+        "q9": Team.objects.filter(
+            all_players__first_name="Samuel",
+            all_players__last_name="Evans"
+        ),
+        "q10": Player.objects.filter(
+            all_teams__location="Manitoba",
+            all_teams__team_name="Tiger-Cats"
+        ),
+        "q11": Player.objects.filter(
+            all_teams__location="Wichita",
+            all_teams__team_name="Vikings"
+        ).exclude(
+            curr_team__location="Wichita",
+            curr_team__team_name="Vikings"
+        ),
+        "q12": Team.objects.filter(
+            all_players__first_name="Jacob",
+            all_players__last_name="Gray"
+        ).exclude(
+            location="Oregon",
+            team_name="Colts"
+        ),
+        "q13": Player.objects.filter(
+            first_name="Joshua",
+            all_teams__league__name="Atlantic Federation of Amateur Baseball Players"
+        ),
+        "q14": Team.objects.annotate(num_players=Count("all_players")).filter(num_players__gte=12),
+        "q15": Player.objects.annotate(num_teams=Count("all_teams")).order_by("-num_teams"),
     }
 
     return render(request, "leagues/index.html", context)
